8550d54f08dcc9b9fb4c0ffada7031e1aafc2927
Laura <laurabande10@gmail.com>
2021-02-03 14:23:14 +0100

    Métod creado en B sin sobreescritura (metodoACambiado)

--- src/main/java/jerarquiaClases/ClaseB.java
+++ src/main/java/jerarquiaClases/ClaseB.java
@@ -1,11 +1,21 @@
 package jerarquiaClases;
 
 public class ClaseB extends ClaseA {
+	
+	private boolean cambiadoEnB;
 
 	// Método que se encarga de inicializar entero a 7
 	@Override
 	public void metodoA() {
 		super.setEntero(7);
 		System.out.println("El valor del entero ha sido cambiado a " + super.getEntero() + " en la clase B");
+	}
+	
+	public void metodoACambiado() {
+		if(super.getEntero() == 7) {
+			cambiadoEnB = true;
+		}else {
+			cambiadoEnB = false;
+		}
 	}
 }
