40c90bc1c7cacb85b97cfa13b6fe399d6e190438
Laura <laurabande10@gmail.com>
2021-02-03 14:35:58 +0100

    Método creado en D sin sobreescritura (metodoALetra)

--- src/main/java/jerarquiaClases/ClaseD.java
+++ src/main/java/jerarquiaClases/ClaseD.java
@@ -2,10 +2,18 @@ package jerarquiaClases;
 
 public class ClaseD extends ClaseA{
 
+	private char caracter;
+	
 	// Método que se encarga de inicializar entero a 9
 	@Override
 	public void metodoA() {
 		super.setEntero(9);
 		System.out.println("El valor del entero ha sido cambiado a " + super.getEntero() + " en la clase D");
 	}
+	
+	public void metodoALetra() {
+		caracter = (char) super.getEntero();
+		System.out.println("El valor del entero actual corresponde con el caracter " + caracter);
+	}
+	
 }
